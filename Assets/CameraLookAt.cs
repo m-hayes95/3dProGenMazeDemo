23dbab61e5d3cb309138c335e37aa1750143001b
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-03-29 11:42:26 +0100

    Added character movement with mouse look

---
+++ Assets/CameraLookAt.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    private float xRotation = 0f;
+
+    public Transform player;
+
+    private void Start()
+    {
+        // Lock cursor into screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Mouse Look at inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Add Y rotation around the X axis (+ Inverts the Y camera).
+        xRotation -= mouseY;
+        // Clamp the rotation so player cant look behind them.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // Rotate player around the X axis.
+        player.Rotate(Vector3.up * mouseX);
+        
+    }
+}
+
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float playerSpeed = 5f;
-    
+
+
 
     private void Update()
     {
-        float moveDirZ = Input.GetAxis("Horizontal");
-        float moveDirX = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3 (-moveDirX, 0f, moveDirZ);
+        // Add horizontal and vertical inputs to local variables.
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // Use x and z axis inputs and assign them to a new vector 3 for movement.
+        Vector3 moveDir = new Vector3(x, 0f, z);
+        // Use the translate method to move the game object, indepentantly from frame rate
         transform.Translate(moveDir * playerSpeed * Time.deltaTime);
     }
-}
+}   
